b83919b820ec6279cbc9dd95121a4790b357af8c
Muhammed ARSLAN <=md.arslan0101@gmail.com>
2021-03-24 00:11:37 +0300

    Add project files.

---
+++ Job Offers Website/Controllers/UsersController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Job_Offers_Website.Entities;
+using Job_Offers_Website.Models;
+
+namespace Job_Offers_Website.Controllers
+{
+    public class UsersController : Controller
+    {
+        private UsersModel usersModel = new UsersModel();
+
+        // GET: Users
+        public ActionResult Index()
+        {
+            ViewBag.users = usersModel.findAll();
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Add()
+        {
+
+            return View("Add" , new Users());
+        }
+
+        [HttpPost]
+        public ActionResult Add(Users users)
+        {
+
+            usersModel.create(users);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Delete(string id)
+        {
+            usersModel.delete(id);
+            return RedirectToAction("Index");
+
+        }
+
+        [HttpGet]
+        public ActionResult Edit(string id)
+        {
+
+            return View("Edit", usersModel.find(id) );
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Users users , FormCollection fc)
+        {
+            string id = fc["Id"];
+            var user = usersModel.find(id);
+            if(users.Password != null)
+            {
+                user.Password = user.Password;
+            }
+            user.Username = users.Username;
+            user.Email = users.Email;
+            user.Age = users.Age;
+            usersModel.update(user);
+            return RedirectToAction("Index");
+
+           
+        }
+    }
+}
---
+++ Job Offers Website/Models/Job.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace Job_Offers_Website.Models
+{
+    public class Job
+    {
+        public int Id { get; set; }
+        [DisplayName("Name Job")]
+        public string JobTitle { get; set; }
+        [DisplayName("Description Job")]
+        [AllowHtml]
+        public string JobContent { get; set; }
+        [DisplayName("Image Job")]
+        public string JobImage { get; set; }
+
+        [DisplayName("Type Job")]
+        public int CategoryId { get; set; }
+
+        public string UserID { get; set; }
+
+        public virtual Category Category { get; set; }
+        public virtual ApplicationUser User { get; set; }
+    }
+}
